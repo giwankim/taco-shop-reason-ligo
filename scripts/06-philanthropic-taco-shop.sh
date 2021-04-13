a361cb97e2f718f8b09b137c90b9c28c10ff5f03
gwk <14240606+giwankim@users.noreply.github.com>
2021-04-13 10:44:52 +0900

    Fixed typo in 06-philanthropic-taco-shop.sh

--- scripts/06-philanthropic-taco-shop.sh
+++ scripts/06-philanthropic-taco-shop.sh
@@ -2,4 +2,4 @@
 ligo dry-run ../contracts/06-philanthropic-taco-shop.religo --amount 1 buy_taco 1n "Map.literal([
   (1n, {current_stock: 50n, max_price: 50tez}),
   (2n, {current_stock: 20n, max_price: 75tez})
-])";
+])"
